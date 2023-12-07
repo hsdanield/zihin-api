f7ae2a67d169f1c139fe5d0a5ac71da0ee62a04b
hsdanield <hsdanield@users.noreply.github.com>
2023-12-07 13:06:26 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="hsdanield"
+original_name="zihin_api"
+original_urlname="zihin-api"
+original_description="Awesome zihin_api created by hsdanield"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv zihin_api $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- zihin_api/app.py
+++ zihin_api/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+zihin_api API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="zihin_api",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://zihin_api.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "hsdanield",
+        "url": "http://zihin_api.com/contact/",
+        "email": "hsdanield@zihin_api.com",
     },
     license_info={
         "name": "The Unlicense",
